9fae37029cd1deabdb453b19e4256343267b55e6
YaOliveira <magicsunshine_ya@msn.com>
2019-11-20 23:40:47 -0300

    aula 1 e aula 2 de python

---
+++ aula1.py
@@ -0,0 +1,18 @@
+'''
+Pode usar aspas simples para comentário
+'''
+# Isso é um comentário
+
+print ('Linha 1')
+print ('Linha 2')
+# Uma linha comentada
+# Segunda linha comentada
+print ('Linha 3')  # Comentário na linha com código tem que ter 2 espaços devido PEP8
+print ('Linha 4')
+print ('Linha 5')
+print ('Linha 6')
+
+"""
+Para documentação e comentários de mais de 2 linhas
+utiliza 3 aspas duplas abrindo e fechando
+"""
---
+++ aula2.py
@@ -0,0 +1,9 @@
+print (123456, end=' # ')
+print ('Luiz','Otávio', 'Maria', 'Xuxu', sep='-', end='')
+print ('Yara')
+
+"""
+824.176.070-18
+"""
+print('824','176', '070', sep='.', end='-')
+print('18')
