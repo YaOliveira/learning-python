208342c8c0ad0eb21a2d0606c092dd0b51bb5da1
YaOliveira <magicsunshine_ya@msn.com>
2019-11-21 23:45:45 -0300

    quebra de linha e print de aspasou barras

--- aula4.py
+++ aula4.py
@@ -7,4 +7,6 @@ print(123456)
 print("Essa é uma 'string'(str).")
 print('Essa é uma "string"(str).')
 print('Esse é meu \'texto\'(str).')
-print('Esse é meu \n texto')
+print('Esse é meu \n texto')  # para quebrar linha
+print(r'Esse é meu \n texto')  # para aparecer a barra
+print(r'Esse é meu \'aspas aqui\'(str).')
