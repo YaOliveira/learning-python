75d0f33aed2502f71fabba02b169587eb0e52db5
YaOliveira <magicsunshine_ya@msn.com>
2019-11-22 00:03:34 -0300

    aula 5

--- aula5.py
+++ aula5.py
@@ -5,12 +5,12 @@ int - inteiro - 123456 - 0 -10 -20 -50 -1500 1500
 float - real/ponto flutuante 10.50 1.5
 bool - booleano - lógico - True or False 10 == 10
 """
-print('Yara', type('Yara'))
-print(type("Yara"))
-print("10", type("10"))
-print(10, type(10))
-print(10 == 10, type(10 == 10))
-print("l" == "L", type("l" == "L"))
-print("L" == "L", type("L" == "L"))
-print(bool(""))
-print(bool("10"))
+print('Yara', type('Yara'))  # str
+print(type("Yara"))  # str
+print("10", type("10"))  # str
+print(10, type(10))  # int
+print(10 == 10, type(10 == 10))  # True bool
+print("l" == "L", type("l" == "L"))  # False bool
+print("L" == "L", type("L" == "L"))  # True bool
+print(bool(""))  # False
+print(bool("10"))  # True
