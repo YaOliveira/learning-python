f30d57c8ab1b44638b5f0233b1ea002922cf6ca3
YaOliveira <magicsunshine_ya@msn.com>
2019-11-21 23:58:28 -0300

    aula 4

--- aula5.py
+++ aula5.py
@@ -1,5 +1,14 @@
 """
-str - string
+Tipos de dados
+str - string - texto - 'Texto' - "Texto"
+int - inteiro - 123456 - 0 -10 -20 -50 -1500 1500
+float - real/ponto flutuante 10.50 1.5
+bool - booleano - lógico - True or False 10 == 10
 """
-print('aspas simples')
-print("aspas duplas")
+print('Yara', type('Yara'))
+print(type("Yara"))
+print("10", type("10"))
+print(10, type(10))
+print(10 == 10, type(10 == 10))
+print("l" == "L", type("l" == "L"))
+print("L" == "L", type("L" == "L"))
