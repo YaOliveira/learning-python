8b814a2297d17518c2cfaaf2e803e22504c7bb89
YaOliveira <magicsunshine_ya@msn.com>
2019-11-24 23:49:21 -0300

    f strings e arredondar casas decimais

--- aula7/aula7.py
+++ aula7/aula7.py
@@ -1,12 +1,12 @@
 """
-Iniciar com letra, pode conter números, separar _, letras minúsculas
+F strings
 """
 nome = 'Yara'
 idade = 36
 altura = 1.43
 e_maior = idade > 18
 peso = 53
-imc = peso // altura ** 2
+imc = peso / altura ** 2
 
 print('Nome:', nome)
 print('Idade:', idade)
@@ -14,3 +14,4 @@ print('Altura:', altura)
 print('É maior:', e_maior)
 print(idade * altura)
 print(nome, 'tem', idade, 'de idade e seu imc é', imc)
+print(f'{nome} tem {idade} de idade e seu imc é {imc:.2f}')
