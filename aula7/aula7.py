8f659b712870090cad96c08b152a83d9e74d44a3
YaOliveira <magicsunshine_ya@msn.com>
2019-11-24 23:57:41 -0300

    subst variaveis e posicionamento

--- aula7/aula7.py
+++ aula7/aula7.py
@@ -14,4 +14,7 @@ print('Altura:', altura)
 print('É maior:', e_maior)
 print(idade * altura)
 print(nome, 'tem', idade, 'de idade e seu imc é', imc)
-print(f'{nome} tem {idade} de idade e seu imc é {imc:.2f}')
+print(f'{nome} tem {idade} de idade e seu imc é {imc:.2f}')  # f strings
+print('{} tem {} anos de idade e seu imc é {:.2f}'.format(nome, idade, imc))  # format
+print('{n} tem {i} anos de idade e seu imc é {m:.2f}'.format(n = nome, i = idade, m = imc))  # format
+print('{0} {0} tem {1} anos de idade e seu imc é {2:.2f}'.format(nome, idade, imc))
